d01d74506f5bd3298e87798b31be51feae340274
CeciliaN99 <cecnyb@net.chalmers.se>
2021-11-03 15:09:44 +0100

    Update helloworld.java

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -3,6 +3,11 @@ package helloworld;
 public class helloworld {
 	public static void main(String[] args) {
 	System.out.println("Hello world!");
+<<<<<<< Updated upstream
 	//Här är min kommentar
 }
 }
+=======
+		String test = "Hello";
+}	}
+>>>>>>> Stashed changes
